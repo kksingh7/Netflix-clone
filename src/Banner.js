5b52710a8856d4f0cb4429fb23f1aae0d3c9688f
kksingh7 <keshavkumarsingh000@gmail.com>
2021-08-03 22:58:14 +0530

    Row.js added

--- src/Banner.js
+++ src/Banner.js
@@ -10,6 +10,7 @@ function Banner() {
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(requests.fetchNetflixOriginals);
+            console.log("hi", request);
             setMovie(
                 request.data.results[
                     Math.floor(Math.random() * request.data.results.length - 1)
@@ -20,8 +21,6 @@ function Banner() {
 
         fetchData();
     }, [])
-
-    console.log(request);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
@@ -35,19 +34,18 @@ function Banner() {
         }}>
             <div className="banner__contents">
                 <h1 className="banner__title">
-                    Movie Name
+                    {movie?.title || movie?.name ||movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    {truncate('This is a test description. Gretel New York-based Gretel were tasked with creating an identity that would connect everything the Netflix brand touches. The result is one of the strongest and most comprehensive identities of recent times. Netflix remains the top choice for TV and movie streaming thanks to its ease of use, lack of commercials and original content. Streaming shows and movies over the internet is the best way to watch TV, and Netflix is the best choice for streaming entertainment, period.', 
-                     150)}    
+                    {truncate(movie?.overview, 150)}    
                 </h1>
             </div>
 
-            <div className="banner--fadebottom" />
+            <div className="banner--fadeBottom" />
         </header>
     )
 }
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import './HomeScreen.css'
-import Nav from './Nav'
-import Banner from './Banner'
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+import Banner from './Banner';
+import requests from "./Requests"; 
+import Row from './Row';
 
 function Homescreen() {
     return (
@@ -10,7 +12,18 @@ function Homescreen() {
 
             <Banner />
 
-            {/* rows */}
+            <Row 
+                title = 'NETFLIX ORIGINALS'
+                fetchUrl = {requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+            <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
         </div>
     )
 }
