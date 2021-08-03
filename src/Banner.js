7915aded451e3ce8bb0375628f6a112267729c86
kksingh7 <keshavkumarsingh000@gmail.com>
2021-08-03 11:14:02 +0530

    new commit

--- src/Banner.js
+++ src/Banner.js
@@ -21,7 +21,7 @@ function Banner() {
         fetchData();
     }, [])
 
-    console.log(movie);
+    console.log(request);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
